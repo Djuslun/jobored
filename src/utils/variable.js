54da5812c6afdf2289cab32a49468b4cf1a66af6
Djuslun <djuslun@yandex.ru>
2023-06-23 10:02:46 +0300

    change api url

--- src/utils/variable.js
+++ src/utils/variable.js
@@ -5,7 +5,7 @@ const authParams = {
   client_secret: 'v3.r.137440105.ffdbab114f92b821eac4e21f485343924a773131.06c3bdbb8446aeb91c35b80c42ff69eb9c457948',
   hr: 0,
   _baseCount: 4,
-  BASE_URL: 'https://startup-summer-2023-proxy.onrender.com/2.0/',
+  BASE_URL: 'https://api.superjob.ru/2.0/',
   x_secret_key: 'GEU4nvd3rej*jeh.eqp',
 }
 
